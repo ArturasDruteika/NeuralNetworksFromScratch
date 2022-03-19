8fc9475b2bbd6fbf2f6252b7bf3272ad4c98d1a8
Arturas Druteika <arturas.druteika@gmail.com>
2022-03-19 17:21:48 +0200

    Implemented ReLu function

---
+++ headers/activations/relu.h
@@ -0,0 +1,4 @@
+#pragma once
+
+std::vector<std::vector<double>> relu(std::vector<std::vector<double>> inputMatrix);
+
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 int main() {
     std::vector<int> labels = {1, 2, 3};
     std::vector<std::vector<double>> inputTensor = {{1, 1, 1, 1},
-                                                    {2, 2, 2, 2},
+                                                    {-2, -2, -2, -2},
                                                     {3, 3, 3, 3}};
 
     int inputShape = inputTensor[1].size();
@@ -19,7 +19,24 @@ int main() {
     Linear linearLayer2(outputNeurons, nClasses);
 
     linearLayer1.forward(inputTensor);
+    linearLayer1.output = relu(linearLayer1.output);
+
+    for (const auto& row : linearLayer1.output) {
+        for (const auto element : row) {
+            std::cout << element << " ";
+        }
+        std::cout << std::endl;
+    }
+
     linearLayer2.forward(linearLayer1.output);
+    linearLayer2.output = relu(linearLayer2.output);
+
+    for (const auto& row : linearLayer2.output) {
+        for (const auto element : row) {
+            std::cout << element << " ";
+        }
+        std::cout << std::endl;
+    }
 
     return 0;
 }
