239d19b41b1b6b9be13e377a040b3bb7b9389a50
Arturas Druteika <arturas.druteika@gmail.com>
2022-03-19 20:40:51 +0200

    Implemented sigmoid function

---
+++ headers/activations/sigmoid.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <vector>
+
+std::vector<std::vector<double>> sigmoid(std::vector<std::vector<double>> inputMatrix);
---
+++ src/activations/sigmoid.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <cmath>
+#include "../../headers/activations/sigmoid.h"
+
+std::vector<std::vector<double>> sigmoid(std::vector<std::vector<double>> inputMatrix) {
+    std::vector<std::vector<double>> resultMatrix;
+
+    for (int i = 0; i < inputMatrix.size(); i++) {
+        std::vector<double> rowVector;
+        rowVector.reserve(inputMatrix[0].size());
+
+        for (int j = 0; j < inputMatrix[0].size(); j++) {
+            rowVector.push_back(1.0 / (1.0 + exp(inputMatrix[i][j])));
+        }
+
+        resultMatrix.push_back(rowVector);
+    }
+
+    return resultMatrix;
+}
