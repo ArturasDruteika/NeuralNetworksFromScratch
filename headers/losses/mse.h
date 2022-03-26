18e576664111824080e7dd049fdea898205016cd
Arturas Druteika <arturas.druteika@gmail.com>
2022-03-26 21:36:27 +0200

    Implemented MSE loss for backward prop

--- headers/losses/mse.h
+++ headers/losses/mse.h
@@ -3,3 +3,4 @@
 #include <vector>
 
 double mseLoss(const std::vector<double> &labels, const std::vector<std::vector<double>> &predictionMatrix);
+std::vector<std::vector<double>> msePrime(const std::vector<double> &labels, const std::vector<std::vector<double>> &predictionMatrix);
--- src/losses/mse.cpp
+++ src/losses/mse.cpp
@@ -14,3 +14,17 @@ double mseLoss(const std::vector<double> &labels, const std::vector<std::vector<
 
     return totalSquaredDifference / predictionMatrix.size();
 }
+
+std::vector<std::vector<double>> msePrime(const std::vector<double> &labels, const std::vector<std::vector<double>> &predictionMatrix)
+{
+    std::vector<std::vector<double>> grad;
+    grad.reserve(labels.size());
+
+    for (int i = 0; i < predictionMatrix.size(); i++)
+    {
+        grad.push_back({2 * (predictionMatrix[i][0] - labels[i]) / labels.size()});
+    }
+
+    return grad;
+
+}
